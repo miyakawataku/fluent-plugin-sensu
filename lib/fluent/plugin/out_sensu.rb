00f1d61b8a6d111646ec2e5e28ca9922cdeb297c
Miyakawa Taku <miyakawa.taku@gmail.com>
2015-07-29 13:14:57 +0900

    check name implementation

--- lib/fluent/plugin/out_sensu.rb
+++ lib/fluent/plugin/out_sensu.rb
@@ -6,5 +6,118 @@ module Fluent
   # Fluentd output plugin to send checks to sensu-client.
   class SensuOutput < Fluent::BufferedOutput
     Fluent::Plugin.register_output('sensu', self)
+
+    #
+    # Config parameters
+    #
+
+    # The IP address or the hostname of the host running sensu-client.
+    config_param :server, :string, :default => 'localhost'
+
+    # The port to which sensu-client is listening.
+    config_param :port, :integer, :default => 3030
+
+    # "name" attribute: default = tag if valid, or "fluent-plugin-sensu"
+    config_param :check_name, :string, :default => nil
+    config_param :check_name_field, :string, :default => nil
+
+    CHECK_NAME_PATTERN = /\A[\w.-]+\z/
+
+    # Load modules.
+    private
+    def initialize
+      super
+      require 'json'
+    end
+
+    # Read and validate the configuration.
+    public
+    def configure(conf)
+      super
+      reject_invalid_check_name
+    end
+
+    # Reject check_name option if invalid
+    private
+    def reject_invalid_check_name
+      if @check_name && @check_name !~ CHECK_NAME_PATTERN
+        raise ConfigError,
+          "check_name must be a string consisting of one or more" +
+          " ASCII alphanumerics, underscores, periods, and hyphens"
+      end
+    end
+
+    # Pack the tuple (tag, time, record).
+    public
+    def format(tag, time, record)
+      [tag, time, record].to_msgpack
+    end
+
+    # Send a check
+    public
+    def write(chunk)
+      results = []
+      chunk.msgpack_each { |(tag, time, record)|
+        payload = {
+          'name' => determine_check_name(tag, record),
+          'output' => record.to_json,
+          'status' => 3,
+          'type' => 'standard',
+          'handlers' => ['default'],
+          'executed' => time
+        }
+        send_check(@server, @port, payload)
+      }
+    end
+
+    # Determines "name" attribute of a check.
+    private
+    def determine_check_name(tag, record)
+      # Field specified by check_name_field option
+      if @check_name_field
+        check_name = record[@check_name_field]
+        if check_name =~ CHECK_NAME_PATTERN
+          return record[@check_name_field]
+        else
+          log.warn('Invalid check name in the field.' +
+                   ' Fallback to check_name option, tag,' +
+                   ' or constant "fluent-plugin-sensu".',
+                   :tag => tag,
+                   :check_name_field => @check_name_field,
+                   :value => check_name)
+          # Fall through
+        end
+      end
+
+      # check_name option
+      if @check_name
+        return @check_name
+      end
+
+      # Tag
+      if tag =~ CHECK_NAME_PATTERN
+        return tag
+      end
+
+      # Default value
+      log.warn('Invalid check name in the tag.' +
+               'Fallback to the constant "fluent-plugin-sensu".',
+               :tag => tag)
+      return 'fluent-plugin-sensu'
+    end
+
+    # Send a check to sensu-client.
+    public
+    def send_check(server, port, payload)
+      json = payload.to_json
+      sensu_client = TCPSocket.open(@server, @port)
+      begin
+        sensu_client.puts(json)
+      ensure
+        sensu_client.close
+      end
+    end
+
   end
+
 end
--- test/plugin/test_out_sensu.rb
+++ test/plugin/test_out_sensu.rb
@@ -1,8 +1,179 @@
 # -*- encoding: utf-8 -*-
-# vim: et sw=2 sts=2
+# vim: et sw=2 sts=2 fdm=marker
 
 require 'helper'
 
 # Test class for Fluent::SensuOutput
 class SensuOutputTest < Test::Unit::TestCase
+
+  # Setup {{{1
+
+  # Sets up the test env and the stub
+  def setup
+    Fluent::Test.setup
+  end
+
+  # Returns a driver of the plugin
+  def create_driver(conf, tag = 'test')
+    driver = Fluent::Test::BufferedOutputTestDriver.new(Fluent::SensuOutput, tag)
+    driver.configure(conf)
+    def (driver.instance).send_check(server, port, payload)
+      @sent_data ||= []
+      @sent_data.push([server, port, payload])
+    end
+    def (driver.instance).sent_data
+      return @sent_data
+    end
+    return driver
+  end
+
+  # }}}1
+  # Connection {{{1
+
+  # Connection settings are read
+  def test_connection_settings
+    driver = create_driver(%[
+      server sensu-client.local
+      port 3031
+    ])
+    assert_equal 'sensu-client.local', driver.instance.instance_eval{ @server }
+    assert_equal 3031, driver.instance.instance_eval{ @port }
+  end
+
+  # Default values are set to connection settings
+  def test_default_connection_settings
+    driver = create_driver('')
+    assert_equal 'localhost', driver.instance.instance_eval{ @server }
+    assert_equal 3030, driver.instance.instance_eval{ @port }
+  end
+
+  # Send default values for empty data and empty setting
+  def test_write_empty_data_with_empty_setting
+    driver = create_driver('', 'ddos')
+    time = Time.parse('2015-01-03 12:34:56 UTC').to_i
+    driver.emit({}, time)
+    driver.run
+    result = driver.instance.sent_data
+    expected = {
+      'name' => 'ddos',
+      'output' => '{}',
+      'status' => 3,
+      'type' => 'standard',
+      'handlers' => ['default'],
+      'executed' => time
+    }
+    assert_equal [['localhost', 3030, expected]], result
+  end
+
+  # Send default values to aother node and another port
+  def test_send_to_another_connection
+    driver = create_driver(%[
+      server sensu-client.local
+      port 3031
+    ], 'ddos')
+    time = Time.parse('2015-01-03 12:34:56 UTC').to_i
+    driver.emit({}, time)
+    driver.run
+    result = driver.instance.sent_data
+    expected = {
+      'name' => 'ddos',
+      'output' => '{}',
+      'status' => 3,
+      'type' => 'standard',
+      'handlers' => ['default'],
+      'executed' => time
+    }
+    assert_equal [['sensu-client.local', 3031, expected]], result
+  end
+
+  # }}}1
+  # "name" attribute {{{1
+
+  def test_rejects_invalid_check_name_in_option
+    assert_raise(Fluent::ConfigError) {
+      create_driver(%[check_name invalid/check/name], 'ddos')
+    }
+  end
+
+  # Determine "name" attribute with options
+  def test_determine_name_attribute_with_options
+    driver = create_driver(%[
+      check_name_field service
+      check_name ddos_detection
+    ], 'ddos')
+    time = Time.parse('2015-01-03 12:34:56 UTC').to_i
+    driver.emit({ 'service' => 'flood' }, time)
+    driver.emit({ 'service' => 'invalid/check/name' }, time)
+    driver.emit({}, time)
+    driver.run
+    result = driver.instance.sent_data
+    expected1 = {
+      'name' => 'flood',
+      'output' => '{"service":"flood"}',
+      'status' => 3,
+      'type' => 'standard',
+      'handlers' => ['default'],
+      'executed' => time
+    }
+    expected2 = {
+      'name' => 'ddos_detection',
+      'output' => '{"service":"invalid/check/name"}',
+      'status' => 3,
+      'type' => 'standard',
+      'handlers' => ['default'],
+      'executed' => time
+    }
+    expected3 = {
+      'name' => 'ddos_detection',
+      'output' => '{}',
+      'status' => 3,
+      'type' => 'standard',
+      'handlers' => ['default'],
+      'executed' => time
+    }
+    assert_equal [
+      ['localhost', 3030, expected1],
+      ['localhost', 3030, expected2],
+      ['localhost', 3030, expected3],
+    ], result
+  end
+
+  # Dertermine "name" attribute with tag
+  def test_determine_name_attribute_with_tag
+    driver = create_driver('', 'ddos')
+    time = Time.parse('2015-01-03 12:34:56 UTC').to_i
+    driver.emit({}, time)
+    driver.run
+    result = driver.instance.sent_data
+    expected = {
+      'name' => 'ddos',
+      'output' => '{}',
+      'status' => 3,
+      'type' => 'standard',
+      'handlers' => ['default'],
+      'executed' => time
+    }
+    assert_equal([['localhost', 3030, expected]], result)
+  end
+
+  # Dertermine "name" attribute as default constant
+  def test_determine_name_attribute_as_default_constant
+    driver = create_driver('', 'invalid/check/name')
+    time = Time.parse('2015-01-03 12:34:56 UTC').to_i
+    driver.emit({}, time)
+    driver.run
+    result = driver.instance.sent_data
+    expected = {
+      'name' => 'fluent-plugin-sensu',
+      'output' => '{}',
+      'status' => 3,
+      'type' => 'standard',
+      'handlers' => ['default'],
+      'executed' => time
+    }
+    assert_equal([['localhost', 3030, expected]], result)
+  end
+
+  # }}}1
+
 end
